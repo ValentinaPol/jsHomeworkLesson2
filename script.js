4a6bcf89985cd81c50b12a36fefde39d3954e97c
Valentina <polianskaya.valentina@gmail.com>
2021-12-16 17:03:36 +0300

    Start

--- script.js
+++ script.js
@@ -1,75 +1,111 @@
-//1. Напишите функцию, которая принимает массив целых чисел и строк. Преобразуйте целые числа в строки и верните новый массив.
-//convertElements([1, 2, "x", "z"]) ➞ ["1", "2", "x", "z"]
-//convertElements(["pdf", 123, "def", 456]) ➞ ["pdf", "123", "def", "456"]
-//convertElements([1, 2, 3, 17, 24, 3, "a", "123b"]) ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
-//convertElements([]) ➞ []
- 
+//1. 1.	Напишите функцию, которая возвращает сумму элементов больше 5 в данном массиве.
+//sumFive([1, 5, 20, 30, 4, 9, 18]) ➞ 77
+//sumFive([1, 2, 3, 4]) ➞ 0
+//sumFive([10, 12, 28, 47, 55, 100]) ➞ 252
+//Используйте для суммы все элементы больше 5, но не элементы которые равны 5 и меньше.
 
-
-function convertElements(arr){
-    var arrResult = [];
-    for (var i = 0; i < arr.length; i++) {
-        arrResult.push(String(arr[i]));
+ function sumFive(arr) {
+    var sum = 0;
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] > 5){
+            sum += arr[i]
+        }
     }
+    return sum;
+ }
+
+ console.log (sumFive([1, 5, 20, 30, 4, 9, 18]));
+ console.log (sumFive([1, 2, 3, 4]));
+ console.log (sumFive([10, 12, 28, 47, 55, 100]));
+
+
+
+// 2. 2.	Напишите функцию, преобразующую объект в массив ключей и значений.
+//objectToArray({
+    //D: 1,
+    //B: 2,
+    //C: 3
+    //}) ➞ [["D", 1], ["B", 2], ["C", 3]]
+    
+    //objectToArray({
+    //likes: 2,
+    //dislikes: 3,
+    //followers: 10
+    //}) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+
+    function objectToArray(obj) {
+        var arrResult = [];
+        for (let key in obj){
+            var arrSub = [];
+            arrSub.push(key);
+            arrSub.push(obj[key]);
+            arrResult.push(arrSub);
+        }
+        return arrResult;
+    }
+
+    console.log (objectToArray({D: 1, B: 2, C: 3}));
+    console.log (objectToArray({likes: 2, dislikes: 3, followers: 10}));
+        
+        
+        
+
+
+// 3. 3.	Массивы могут включать в себя разные типы данных. Ваша задача  - просуммировать все числовые элементы в данном массиве. Создайте функцию, которая принимает массив и возвращает сумму всех чисел в массиве.
+//numbersSum([1, 2, "13", "4", "645"]) ➞ 3
+//numbersSum([true, false, "123", "75"]) ➞ 0
+//numbersSum([1, 2, 3, 4, 5, true]) ➞ 15
+
+
+function numbersSum(arr){
+    var sum = 0;
+    for (i = 0; i < arr.length; i++){
+        if (typeof arr[i] == 'number'){
+            sum += arr[i]
+        } 
+    }
+    return sum;
+}
+ console.log (numbersSum([1, 2, "13", "4", "645"]));
+ console.log (numbersSum([true, false, "123", "75"]));
+ console.log (numbersSum([1, 2, 3, 4, 5, true]));
+
+
+
+// 4. Создайте функцию, которая принимает объект и возвращает ключи и значения в виде отдельных массивов. Возвращает ключи, отсортированные в алфавитном порядке, и их соответствующие значения в том же порядке.
+//keysAndValues({ a: 1, b: 2, c: 3 })➞ [["a", "b", "c"], [1, 2, 3]]
+//keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" })➞ [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]
+//keysAndValues({ key1: true, key2: false, key3: undefined })➞ [["key1", "key2", "key3"], [true, false, undefined]]
+
+function keysAndValues(obj){
+    var arrResult = [];
+    var arrSub1 = [];
+    var arrSub2 = [];
+
+    for (var key in obj){
+        arrSub1.push(key);  
+    }
+
+    arrSub1.sort();
+
+    for (i = 0; i < arrSub1.length; i++){
+        arrSub2.push(obj[arrSub1[i]]);
+    }
+
+    arrResult.push(arrSub1); 
+    arrResult.push(arrSub2); 
     return arrResult;
 }
 
-console.log(convertElements([1, 2, "x", "z"]));
-console.log(convertElements(["pdf", 123, "def", 456]));
-console.log(convertElements([1, 2, 3, 17, 24, 3, "a", "123b"]));
-console.log(convertElements([]));
+console.log (keysAndValues({ a: 1, b: 2, c: 3 }));
+console.log (keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" }));
+console.log (keysAndValues({ key1: true, key2: false, key3: undefined }));
 
 
 
 
-// 2. Создайте функцию, которая возвращает количество " true” значений в массиве.
-//numericTrue([true, false, false, true, false]) ➞ 2
-//numericTrue ([false, false, false, false]) ➞ 0
-//numericTrue ([]) ➞ 0
-//Вернуть 0, если дан пустой массив.
-//Все элементы массива имеют тип bool (true или false).
-
-function numericTrue(arr){
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === true){
-            sum++;
-        }
-    }
-    return sum; 
-}
-
-console.log(numericTrue([true, false, false, true, false]));
-console.log(numericTrue([false, false, false, false]));
-console.log(numericTrue([]));
 
 
-
-// 3. Вернуть наибольшее и наименьшее числа
-//Создайте функцию, которая принимает массив чисел, и возвращает наибольшее и наименьшее число.
-//highLow([1, 2, 3, 4, 5]) ➞ 5, 1
-//highLow([1, 2, -3, 4, 5]) ➞ 5. -3
-//highLow([1, 9, 3, 4, –5]) ➞ 9, -5
-//highLow([13]) ➞ 13, 13
-
-
-function highLow(arr){
-    var result = {max: arr[0], min: arr[0]}
-    for (var i = 1; i < arr.length; i++){
-        if (result.max < arr[i]){
-            result.max = arr[i];
-        } 
-        if (result.min > arr[i]){
-            result.min = arr[i];
-        } 
-    }
-    return result;
-}
-
-console.log (highLow([1, 2, 3, 4, 5]));
-console.log (highLow([1, 2, -3, 4, 5]));
-console.log (highLow([1, 9, 3, 4, -5]));
-console.log (highLow([13]));
 
 
 
